2a2a1086c79ce469b31b619fc5d83c198f7e97a4
Vinay <vinay@Vinays-MacBook-Pro.local>
2026-03-22 23:24:07 -0700

    updated title string

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className='App-title'>Welcome to React</h1>
+        <h1 className='App-title'>Fibonacci Calculator v2</h1>
          <Router>
           <div>
             <Link to="/">Home</Link>
